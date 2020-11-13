1e2ed57912ad268cdd588a83bc3bbe9a5aa0a188
DimitrisPatiniotis <dimpatspy@gmail.com>
2020-11-13 18:15:21 +0200

    war game

--- war.py
+++ war.py
@@ -50,3 +50,72 @@ class Player():
 
     def __str__(self):
         return f'Player {self.name} has {len(self.all_cards)} cards.'
+
+player_one = Player("One")
+player_two = Player("Two")
+
+new_deck = Deck()
+new_deck.shuffle()
+
+for x in range(26):
+    player_one.add_cards(new_deck.deal_one())
+    player_two.add_cards(new_deck.deal_one())
+
+game_on = True
+round_num = 0
+
+while game_on:
+
+    round_num += 1
+    print(f'Round {round_num}')
+
+    if len(player_one.all_cards) == 0:
+        print("Player Two wins")
+        game_on = False
+        break
+
+    if len(player_two.all_cards) == 0:
+        print("Player One wins")
+        game_on = False
+        break
+
+    player_one_cards = []
+    player_one_cards.append(player_one.remove_one())
+
+    player_two_cards = []
+    player_two_cards.append(player_two.remove_one())
+
+    at_war = True
+    while at_war:
+
+        if player_one_cards[-1].value > player_two_cards[-1].value:
+            player_one.add_cards(player_one_cards)
+            player_one.add_cards(player_two_cards)
+
+            at_war = False
+
+        elif player_one_cards[-1].value < player_two_cards[-1].value:
+            player_two.add_cards(player_one_cards)
+            player_two.add_cards(player_two_cards)
+
+            at_war = False
+        
+        else:
+            print("It's a war!")
+
+            if len(player_one.all_cards) < 3:
+                print("Player One unable to declare war")
+                print("Player Tow wins")
+                game_on = False
+                break
+
+            elif len(player_two.all_cards) < 3:
+                print("Player Two unable to declare war")
+                print("Player One wins")
+                game_on = False
+                break
+
+            else:
+                for num in range(3):
+                    player_one_cards.append(player_one.remove_one())
+                    player_two_cards.append(player_two.remove_one())
